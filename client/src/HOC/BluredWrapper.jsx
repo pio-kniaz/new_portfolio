cd32b82c00b18a45108c31df7d1df88d1c267adc
pio-kniaz <pio.kniaz@gmail.com>
2019-05-18 11:07:31 +0200

    Animation on home page fixed

---
+++ client/src/HOC/BluredWrapper.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BluredWrapper = (props) => {
+return (
+  <div className="janusz">
+    {props}
+  </div>
+)
+};
+
+export default BluredWrapper;
--- client/src/page/AboutPage.jsx
+++ client/src/page/AboutPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import MainLayout from 'components/layout/component/main/MainLayout';
 import AboutContainer from 'components/about/container/AboutContainer';
+import BluredWrapper from 'HOC/BluredWrapper';
 
 const AboutPage = () => (
   <>
